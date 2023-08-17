d1b5e39ffbcca253fb97e8399434523d45641f86
sy0725 <sunyong0725@naver.com>
2023-08-17 15:51:46 +0900

    0817 수업 내용

--- src/main.jsx
+++ src/main.jsx
@@ -3,32 +3,6 @@ import "./styles/tailwind.css";
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createBrowserRouter } from "react-router-dom";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter(
-  // Routes
-  [
-    // Routes Object
-    {
-      path: "/",
-      element: <Layout />,
-      children : [
-        { path : 'CTA' , element: <CTA/>}
-      ]
-    },
-  ]
-);
-
-
-Function CTA (){
-  return (
-    <div>
-      <abbr title=""></abbr>
-    </div>
-  )
-}
 
 // ReactDOMRoot { render, unmount }
 ReactDOM.createRoot(document.getElementById("root")).render(
